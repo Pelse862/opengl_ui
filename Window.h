301f23f61e10530620759ef98a095f701edf877d
pelse862 <pelse862@student.liu.se>
2019-01-03 16:51:58 +0100

    Changed some helper code

--- Window.h
+++ Window.h
@@ -9,21 +9,31 @@ public:
 	Window(int w, int h, std::string n) :width(w), height(h), name(n){}
 	GLFWwindow* getWindow(){
 		
-		return glfwCreateWindow(width, height, name.c_str(), nullptr, nullptr);;
-	}
-	bool setWindow(GLFWwindow* w) {
+		GLFWwindow* w = glfwCreateWindow(width, height, name.c_str(), nullptr, nullptr);
 		if (w == nullptr)
 		{
 			std::cout << "Failed to create GLFW window" << std::endl;
 			glfwTerminate();
+			return nullptr;
+		}
+		return w;
+	}
+	bool isWindowSet(GLFWwindow* w) {
+		if (w)
+		{
+			std::cout << "Window is not set Properly" << std::endl;
+			glfwTerminate();
 			return false;
 		}
-		return true;;
+		return true;
 	}
 
-	~Window() = default;
+	~Window() {
+		delete w;
+	};
 
 private:
+	GLFWwindow* w;
 	int width;
 	int height;
 	std::string name;
